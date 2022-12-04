480a1a6c405177a6f984466509977ac3074ef710
Billal <billal78344@gmail.com>
2022-12-04 21:00:21 +0100

    Final commit

---
+++ assets.ts
@@ -0,0 +1,288 @@
+export const css  = `body {
+                width: 1024px;
+                height: 1024px;
+                margin:0;
+              }
+              #background_image{
+                position:absolute;
+                width: 1024px;
+                height: 1024px;
+              }
+              #player_image{
+                position:absolute;
+                width:400px;
+                left:620px;
+                top:465px;
+              }
+            
+              .0_2 {
+                overflow:hidden;
+            }
+            .e0_2 {
+            
+                width:1024px;
+                height:1024px;
+                position:absolute;
+            }
+            .e1_3 {
+                width:1024px;
+                height:1024px;
+                position:absolute;
+                left:0px;
+                top:0px;
+                background-repeat:no-repeat;
+                background-size:cover;
+            }
+            .e0_8 {
+                color:rgba(195.9998819231987, 38.999955877661705, 174.00002002716064, 1);
+                width:662px;
+                height:152px;
+                position:absolute;
+                left:81px;
+                top:109px;
+                font-family:United Sans Reg;
+                text-align:left;
+                font-size:65px;
+                letter-spacing:0;
+                background-color:transparent;
+            }
+            .e0_9 {
+                color:rgba(0, 0, 0, 1);
+                width:421px;
+                height:28px;
+                position:absolute;
+                left:81px;
+                top:817px;
+                font-family:United Sans Reg;
+                text-align:left;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_10 {
+                color:rgba(0, 0, 0, 1);
+                width:421px;
+                height:28px;
+                position:absolute;
+                left:81px;
+                top:764px;
+                font-family:United Sans Reg;
+                text-align:left;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_11 {
+                color:rgba(0, 0, 0, 1);
+                width:421px;
+                height:28px;
+                position:absolute;
+                left:81px;
+                top:710px;
+                font-family:United Sans Reg;
+                text-align:left;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_12 {
+                color:rgba(0, 0, 0, 1);
+                width:421px;
+                height:28px;
+                position:absolute;
+                left:81px;
+                top:656px;
+                font-family:United Sans Reg;
+                text-align:left;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_13 {
+                color:rgba(0, 0, 0, 1);
+                width:421px;
+                height:28px;
+                position:absolute;
+                left:81px;
+                top:604px;
+                font-family:United Sans Reg;
+                text-align:left;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_14 {
+                color:rgba(0, 0, 0, 1);
+                width:421px;
+                height:28px;
+                position:absolute;
+                left:81px;
+                top:549px;
+                font-family:United Sans Reg;
+                text-align:left;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_15 {
+                color:rgba(0, 0, 0, 1);
+                width:421px;
+                height:28px;
+                position:absolute;
+                left:81px;
+                top:495px;
+                font-family:United Sans Reg;
+                text-align:left;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_16 {
+                color:rgba(0, 0, 0, 1);
+                width:421px;
+                height:28px;
+                position:absolute;
+                left:81px;
+                top:444px;
+                font-family:United Sans Reg;
+                text-align:left;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_17 {
+                color:rgba(0, 0, 0, 1);
+                width:421px;
+                height:28px;
+                position:absolute;
+                left:81px;
+                top:388px;
+                font-family:United Sans Reg;
+                text-align:left;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_18 {
+                color:rgba(0, 0, 0, 1);
+                width:421px;
+                height:28px;
+                position:absolute;
+                left:81px;
+                top:334px;
+                font-family:United Sans Reg;
+                text-align:left;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_19 {
+                color:rgba(255, 255, 255, 1);
+                width:116px;
+                height:28px;
+                position:absolute;
+                left:501px;
+                top:821px;
+                font-family:United Sans Reg;
+                text-align:center;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_20 {
+                color:rgba(255, 255, 255, 1);
+                width:116px;
+                height:28px;
+                position:absolute;
+                left:501px;
+                top:767px;
+                font-family:United Sans Reg;
+                text-align:center;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_21 {
+                color:rgba(255, 255, 255, 1);
+                width:116px;
+                height:28px;
+                position:absolute;
+                left:501px;
+                top:713px;
+                font-family:United Sans Reg;
+                text-align:center;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_22 {
+                color:rgba(255, 255, 255, 1);
+                width:116px;
+                height:28px;
+                position:absolute;
+                left:501px;
+                top:658px;
+                font-family:United Sans Reg;
+                text-align:center;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_23 {
+                color:rgba(255, 255, 255, 1);
+                width:116px;
+                height:28px;
+                position:absolute;
+                left:501px;
+                top:606px;
+                font-family:United Sans Reg;
+                text-align:center;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_24 {
+                color:rgba(255, 255, 255, 1);
+                width:116px;
+                height:28px;
+                position:absolute;
+                left:501px;
+                top:551px;
+                font-family:United Sans Reg;
+                text-align:center;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_25 {
+                color:rgba(255, 255, 255, 1);
+                width:116px;
+                height:28px;
+                position:absolute;
+                left:501px;
+                top:495px;
+                font-family:United Sans Reg;
+                text-align:center;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_26 {
+                color:rgba(255, 255, 255, 1);
+                width:116px;
+                height:28px;
+                position:absolute;
+                left:501px;
+                top:442px;
+                font-family:United Sans Reg;
+                text-align:center;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_27 {
+                color:rgba(255, 255, 255, 1);
+                width:116px;
+                height:28px;
+                position:absolute;
+                left:501px;
+                top:388px;
+                font-family:United Sans Reg;
+                text-align:center;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }
+            .e0_28 {
+                color:rgba(255, 255, 255, 1);
+                width:116px;
+                height:28px;
+                position:absolute;
+                left:501px;
+                top:334px;
+                font-family:United Sans Reg;
+                text-align:center;
+                font-size:28.422046661376953px;
+                letter-spacing:0;
+            }`;
